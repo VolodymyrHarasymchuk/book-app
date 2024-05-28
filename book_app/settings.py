f4705739ee50d7b73e9d72de35e5e3aebaa56a69
Volodymyr <vova2003869@gmail.com>
2024-05-28 22:11:28 +0300

    connected to the database, created models

--- book_app/settings.py
+++ book_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books.apps.BooksConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'book_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db', 
+        'USER': 'postgres',
+        'PASSWORD': 'djdf3820',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kyiv'
 
 USE_I18N = True
 
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'books.User'
--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    type = models.CharField(max_length=20)
+    bio_text = models.CharField(max_length=1000)
+
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=400)
+    isbn = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    date_pub = models.DateTimeField("book publishing date")
+    rating = models.IntegerField(default=1)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    date_pub = models.DateTimeField("review publishing date")
+    rating = models.IntegerField(default=1)
