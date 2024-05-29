b1de347547d4ffb6cae074a447eb38c6af43185d
Volodymyr <vova2003869@gmail.com>
2024-05-29 14:01:07 +0300

    minor changes

--- books/views.py
+++ books/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from .models import Book
 from .forms import CustomUserCreationForm
 
 def index(request):
+#    latest_question_list = Book.objects.filter(pub_date__lte=timezone.now()).exclude(choice__isnull=True).order_by("pub_date")
+#    context = { "latest_question_list": latest_question_list }
     return render(request, "books/index.html")
 
 def sign_up(request):
