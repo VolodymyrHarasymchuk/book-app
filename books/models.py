16ccc74b7ca79b4f95145b6cc26a76b74d37df2f
Volodymyr <vova2003869@gmail.com>
2024-05-29 11:34:19 +0300

    changed models: added date_posted to book and choices to user

--- books/models.py
+++ books/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    type = models.CharField(max_length=20)
+    type = models.CharField(max_length=20, choices={"reader": "reader", "author": "author"})
     bio_text = models.CharField(max_length=1000)
 
 class Book(models.Model):
@@ -11,11 +13,12 @@ class Book(models.Model):
     isbn = models.IntegerField()
     description = models.CharField(max_length=1000)
     date_pub = models.DateTimeField("book publishing date")
+    date_posted = models.DateTimeField(default=timezone.now)
     rating = models.IntegerField(default=1)
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     text = models.CharField(max_length=1000)
-    date_pub = models.DateTimeField("review publishing date")
+    date_posted = models.DateTimeField(default=timezone.now)
     rating = models.IntegerField(default=1)
