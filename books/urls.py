ef9044fd354e9af5d19d9ec06d31fa71f818029d
Volodymyr <vova2003869@gmail.com>
2024-05-29 03:11:22 +0300

    finished sign up page

--- books/urls.py
+++ books/urls.py
@@ -6,6 +6,6 @@ app_name = "books"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("sign_up", views.index, name="sign_up"),
+    path("sign_up", views.sign_up, name="sign_up"),
     path("logout_user/", TemplateView.as_view(template_name='registration/logout_user.html'), name='logout_user'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserCreationForm
 
 def index(request):
     return render(request, "books/index.html")
 
 def sign_up(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return HttpResponseRedirect(reverse("main:index"))
+            return HttpResponseRedirect(reverse("books:index"))
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     
     return render(request, "registration/sign_up.html", {"form": form})
