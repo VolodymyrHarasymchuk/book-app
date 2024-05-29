5438d6588aed86df606ba5092f31d598faf0e1ac
Volodymyr <vova2003869@gmail.com>
2024-05-29 14:18:46 +0300

    customized admin for books

--- books/admin.py
+++ books/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import User, Book, Review
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "date_posted"]
+
 admin.site.register(User)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Review)
